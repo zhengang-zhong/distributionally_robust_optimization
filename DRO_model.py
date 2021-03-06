0d9875c548552fbeb826ce177b2e9a1e70b76aeb
zzg <zhengang.zhong@gmail.com>
2021-03-06 20:30:17 +0000

    updated

--- DRO_model.py
+++ DRO_model.py
@@ -29,7 +29,7 @@ class Model:
         self.D = D
         self.E = E
 
-        n, m, d, r, Nx, Nu, Ny, Nw, Ax, Bx, Cx, Ay, By, Cy, Ey, Cx_tilde, Cy_tilde, Ey_tilde, D_tilde = self.stack_system(Ak, Bk, C, D, E, x_init, N)
+        n, m, d, r, Nx, Nu, Ny, Nw, Ax, Bx, Cx, Ay, By, Cy, Ey, Cx_tilde, Cy_tilde, Ey_tilde, D_tilde  = self.stack_system(Ak, Bk, C, D, E, x_init, N)
         # Define relevant dimension
         self.n = n
         self.m = m
@@ -139,10 +139,10 @@ class Model:
         for i in range(N):
             Ey[i * r: (i + 1) * r, i * d: (i + 1) * d] = E
         # Cx_tilde
-        Cx_tilde[:, 0: 1] = Ax @ x_init
+        Cx_tilde[:, [0]] = Ax @ x_init
         Cx_tilde[:, 1:] = Cx
         # Cy_tilde
-        Cy_tilde[r:, 0: 1] = Ay @ x_init
+        Cy_tilde[r:, [0]] = Ay @ x_init
         Cy_tilde[r:, 1:] = Cy
         # Ey_tilde
         Ey_tilde[0, 0] = 1
@@ -155,7 +155,7 @@ class Model:
 
 
 
-    def change_xinit(self, x_init):
+    def change_xinit(self, x_k):
         '''
         x_next = A x_k + B u_k + C w_k
         y_k = D x_k + E w_k
@@ -174,81 +174,36 @@ class Model:
 
         N = self.N
 
-        Ax = np.zeros([Nx, n])
-        Bx = np.zeros([Nx, Nu])
-        Cx = np.zeros([Nx, Nw])
-        Ay = np.zeros([Ny, n])
-        By = np.zeros([Ny, Nu])
-        Cy = np.zeros([Ny, Nw])
-        Ey = np.zeros([Ny, Nw])
-        Cx_tilde = np.zeros([Nx, Nw + 1])
-        Cy_tilde = np.zeros([Ny + 1, Nw + 1])
-        Ey_tilde = np.zeros([Ny + 1, Nw + 1])
-        D_tilde = np.zeros([Ny + 1, Nx])
-
         A = self.Ak
         B = self.Bk
         C = self.C
         D = self.D
         E = self.E
 
-        # Ax
-        for i in range(N + 1):
-            Ax[i * n:(i + 1) * n, :] = matrix_power(A, i)
-        # Bx
-        for i in range(N):
-            mat_temp = B
-            for j in range(i + 1):
-                Bx[(i + 1) * n: (i + 2) * n, (i - j) * m: (i - j + 1) * m] = mat_temp
-                mat_temp = A @ mat_temp
-        # Cx
-        for i in range(N):
-            mat_temp = C
-            for j in range(i + 1):
-                Cx[(i + 1) * n: (i + 2) * n, (i - j) * d: (i - j + 1) * d] = mat_temp
-                mat_temp = A @ mat_temp
-        # Ay
-        for i in range(N):
-            Ay[i * r:(i + 1) * r, :] = D @ matrix_power(A, i)
-            # By
-        for i in range(N):
-            mat_temp = B
-            for j in range(i + 1):
-                By[(i + 1) * r: (i + 2) * r, (i - j) * m: (i - j + 1) * m] = D @ mat_temp
-                mat_temp = A @ mat_temp
-        # Cy
-        for i in range(N):
-            mat_temp = C
-            for j in range(i + 1):
-                Cy[(i + 1) * r: (i + 2) * r, (i - j) * d: (i - j + 1) * d] = D @ mat_temp
-                mat_temp = A @ mat_temp
-        # Ey
-        for i in range(N):
-            Ey[i * r: (i + 1) * r, i * d: (i + 1) * d] = E
-        # Cx_tilde
-        Cx_tilde[:, [0]] = Ax @ x_init
-        Cx_tilde[:, 1:] = Cx
-        # Cy_tilde
-        Cy_tilde[r:, [0]] = Ay @ x_init
-        Cy_tilde[r:, 1:] = Cy
-        # Ey_tilde
-        Ey_tilde[0, 0] = 1
-        Ey_tilde[1:, 1:] = Ey
-        # D_tilde
-        for i in range(N):
-            D_tilde[1 + i * r: 1 + (i + 1) * r, i * d: (i + 1) * d] = D
 
+        Ax = self.Ax
+        Bx = self.Bx
+        Cx = self.Cx
+        Ay = self.Ay
+        By = self.By
+        Cy = self.Cy
+        Ey = self.Ey
+
+        Cx_tilde = self.Cx_tilde
+        Cy_tilde = self.Cy_tilde
+        Ey_tilde = self.Ey_tilde
+        D_tilde = self.D_tilde
+
+        # Cx_tilde
+        Cx_tilde[:, [0]] = Ax @ x_k
+        # Cx_tilde[:, 1:] = Cx
+        # Cy_tilde
+        Cy_tilde[r:, [0]] = Ay @ x_k
+        # Cy_tilde[r:, 1:] = Cy
 
         # Stack system matrices
-        self.Ax = Ax
-        self.Bx = Bx
-        self.Cx = Cx
-        self.Ay = Ay
-        self.By = By
-        self.Cy = Cy
-        self.Ey = Ey
+
         self.Cx_tilde = Cx_tilde
         self.Cy_tilde = Cy_tilde
-        self.Ey_tilde = Ey_tilde
-        self.D_tilde = D_tilde
+
         # return n, m, d, r, Nx, Nu, Ny, Nw, Ax, Bx, Cx, Ay, By, Cy, Ey, Cx_tilde, Cy_tilde, Ey_tilde, D_tilde
